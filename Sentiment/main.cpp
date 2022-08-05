ebdd7c564db0cabeb1a27d7774f425727f014afd
Яшин Прохор <iashin.pa@phystech.edu>
2022-08-05 09:42:59 +0300

    #2 Feature: modes for labelling

--- Sentiment/main.cpp
+++ Sentiment/main.cpp
@@ -8,7 +8,28 @@ int main(int argc, char** argv)
 {
 	try
 	{
-		sentiment::System().run();
+		std::string mode;
+
+		std::cout << "Possible modes: \n\t1. update-binance\n\t2. label\n\t3. label-study\n\t4. default" << std::endl;
+		std::cout << "Mode: ";
+		std::cin >> mode;
+
+		if (mode == "update-binance")
+		{
+			sentiment::System().update_binance();
+		}
+		else if (mode == "label")
+		{
+			sentiment::System().label();
+		}
+		else if (mode == "label-study")
+		{
+			sentiment::System().label_study();
+		}
+		else
+		{
+			sentiment::System().run();
+		}
 	}
 	catch (const std::exception& e)
 	{
